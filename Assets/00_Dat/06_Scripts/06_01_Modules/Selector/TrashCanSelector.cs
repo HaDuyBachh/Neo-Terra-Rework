e796a6dd8aad20adfb8ef69e1d8f459a323f46f6
TuanDat <163112024+DatLe26090806@users.noreply.github.com>
2024-09-03 21:30:55 +0700

    add trash can into scene

--- Assets/00_Dat/06_Scripts/06_01_Modules/Selector/TrashCanSelector.cs
+++ Assets/00_Dat/06_Scripts/06_01_Modules/Selector/TrashCanSelector.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Modules.Selector
 {
-    public enum SelectorType { 
+    public enum SelectorType
+    {
         ByMouse,
         ByHand,
     }
@@ -13,18 +15,49 @@ namespace Modules.Selector
         [Header("By Hand")]
         public GameObject handPivot;
 
-        public void Update(){
-            Vector3 pivotPosition = GetPivotPosition();
-            Vector3 selectDirection = GetSelectDirection();
-        
+        private Transform _currentTrashCan;
+
+        public void Update()
+        {
+
+            // Tạo một tia từ camera đi qua điểm chuột
+            Ray ray = GetRay();
+
+            // Bắn raycast
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                // Xử lý khi raycast trúng một đối tượng
+                if (hit.collider.gameObject.CompareTag("trashAndCanIcon"))
+                {
+                    if (_currentTrashCan != null) return;
+                    _currentTrashCan = hit.collider.gameObject.transform.parent.parent;
+                    _currentTrashCan.GetComponent<Outline>().enabled = true;
+                    return;
+                }
+            }
+
+
+            // Xử lý khi raycast không trúng đối tượng nào
+            if (_currentTrashCan != null)
+            {
+                _currentTrashCan.GetComponent<Outline>().enabled = false;
+            }
+            _currentTrashCan = null;
+
+
         }
 
-        private Vector3 GetPivotPosition(){
-            return default;
-        }
-
-        private Vector3 GetSelectDirection(){
-            return default;
+        private Ray GetRay()
+        {
+            switch (selectorType)
+            {
+                case SelectorType.ByMouse:
+                    return Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+                case SelectorType.ByHand:
+                    return new Ray(handPivot.transform.position, handPivot.transform.forward);
+                default:
+                    return new Ray();
+            }
         }
     }
 }
