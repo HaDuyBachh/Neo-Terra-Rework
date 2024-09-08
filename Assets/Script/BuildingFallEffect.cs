d668fd8f1dc1c05998207e907dc0ec228b4e05d5
Ha Duy Bach <111235354+HaDuyBachh@users.noreply.github.com>
2024-09-08 09:54:30 +0300

    Update trap màn 2

---
+++ Assets/Script/BuildingFallEffect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingFallEffect : MonoBehaviour, TrapEffect
+{
+    public Vector3 rotation;
+    public float fallTime = 1f;
+    public void Start()
+    {
+        Active();
+    }
+    public void Active()
+    {
+        StartCoroutine(SetFall());
+    }
+
+    IEnumerator SetFall()
+    {
+        Debug.Log(transform.eulerAngles + "   " + rotation);
+        while (transform.rotation != Quaternion.Euler(rotation))
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(rotation), fallTime * Time.deltaTime);
+            yield return null;
+        }
+    }
+}
